075c9626476a774663b8315b2854bb4136796cde
katayama <tattu.0310@gmail.com>
2022-06-23 16:38:22 +0900

    ⚡️ toast追加

--- lib/main.dart
+++ lib/main.dart
@@ -96,6 +96,7 @@ class _B2EPageState extends State<B2EPage> {
       int start = ret.indexOf("employeeNo");
       employeeNo = ret.substring(
           start + "employeeNo".length + 3, start + "employeeNo".length + 9);
+      ToastService.showSuccessToast("登録完了しました");
       pushDashBoardPage();
     }
   }
